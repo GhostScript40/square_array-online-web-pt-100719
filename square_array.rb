4a5019da16dbb8d2d9778751f8a87be1b5e3a31d
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-25 01:02:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,14 @@
 require 'pry'
 def square_array(array)
+<<<<<<< HEAD
   square_numbers = []
   array.each do |square| 
     square_numbers << square **2 
    end
    square_numbers
+=======
+  array.each do |square| square = square **2 
+  array.push(square)
+    end
+>>>>>>> 3ce624bbeb84ddd69a4a82b6f114c5269f10e1ab
 end
